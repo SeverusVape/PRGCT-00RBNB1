bb970b0bea7a3ee4cf059fc5d42ccc5ee4fe11a0
Veter v Golove <koshelevworkmail@gmail.com>
2023-01-25 18:52:36 -0500

    Log in page

--- client/src/Pages/LoginPage.jsx
+++ client/src/Pages/LoginPage.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
-    return <div>LoginPage</div>;
+    return (
+        <div className="mt-4 grow flex items-center justify-around">
+            <div className="mb-60">
+                <h1 className="text-3xl text-center mb-4">Login</h1>
+                <form className="max-w-md mx-auto">
+                    <input type="email" placeholder="your@email.com" required />
+                    <input
+                        type="password"
+                        placeholder="your_password"
+                        required
+                    />
+                    <button className="login-btn">Log In</button>
+                    <div className=" text-center py-2 text-gray-600">
+                        {" "}
+                        Don't have an account yet?{" "}
+                        <Link
+                            className="underline text-rose-500"
+                            to={"/register"}
+                        >
+                            Register now!
+                        </Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default LoginPage;
